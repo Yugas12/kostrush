afb194e8f37acab1836f7a45a26d47f07b92f870
Yugas12 <wahyuprastyo907@gmail.com>
2024-05-21 11:24:23 +0700

    finishing

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
-import 'package:kostrushapp/Login.dart';
-import 'package:kostrushapp/Page/home_page.dart';
-import 'package:kostrushapp/SplashScreen.dart';
-import 'package:kostrushapp/TentangKami.dart';
+import 'package:kostrushapp/presentation/themes/main_theme.dart';
+import 'package:kostrushapp/res/routes/app_pages.dart';
 
+import 'di/application_module.dart';
 
-void main() {
+// late final PackageInfo packageInfo;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // packageInfo = await PackageInfo.fromPlatform();
+
+  await initApplicationModule();
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: SplashScreen(),
+      theme: MainTheme.lightTheme(context),
+      getPages: AppPages.routes,
+      initialRoute: AppPages.initial,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale.fromSubtags(languageCode: 'en', countryCode: 'US'),
+        Locale.fromSubtags(languageCode: 'id', countryCode: 'ID'),
+      ],
     );
   }
 }
